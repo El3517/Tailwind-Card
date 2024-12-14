e0de8434dc21d4c468c1dc60790d0b3b076a58c4
Eneje Caleb <enejecaleb@gmail.com>
2024-12-14 08:29:48 +0100

    Gotten a hang of tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card rounded mx-96 mt-5">This is a card made with tailwind css</div>
+    </div>
+  )
+}
+
+export default App
